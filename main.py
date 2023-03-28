41faf971a8f41c84a097db9671d75054a7b8ee20
Muhammad Talha Tahir <talhatahir@pk.folio3.com>
2023-03-28 18:59:05 +0500

    get_books method now returns the BOOKS constant

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {"title": "Physics", "author": "Some random dude", "category": "science"},
+    {"title": "Computer", "author": "Another random dude", "category": "Science"},
+    {"title": "English", "author": "Some british guy", "category": "Literature"},
+]
 
-@app.get("/")
-async def hello_world():
-    return {"message": "Hello_world"}
+
+@app.get("/books")
+async def get_books():
+    return BOOKS
