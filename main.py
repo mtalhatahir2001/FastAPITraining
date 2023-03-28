50840c09b5e6b06f1a8446950edd1bcf5e833ac2
Muhammad Talha Tahir <talhatahir@pk.folio3.com>
2023-03-28 19:27:16 +0500

    path parameter and query parameter

--- main.py
+++ main.py
@@ -12,3 +12,20 @@ BOOKS = [
 @app.get("/books")
 async def get_books():
     return BOOKS
+
+
+@app.get("/books/{book_title}")
+async def get_book_by_name(book_title: str):
+    for i in BOOKS:
+        if i.get("title").lower() == book_title.lower():
+            return i
+    return {"error": "book not found!"}
+
+
+@app.get("/books/")
+async def filter_books(category: str):
+    filtered_list = list()
+    for i in BOOKS:
+        if i.get("category").lower() == category.lower():
+            filtered_list.append(i)
+    return filtered_list
