dcef8316f223d97b6f7b2ce820964eebf94fc6b2
Muhammad Talha Tahir <talhatahir@pk.folio3.com>
2023-03-29 11:51:15 +0500

    Solved the assignment in Section 8

--- project_2.py
+++ project_2.py
@@ -8,13 +8,20 @@ app = FastAPI()
 
 class Book:
     def __init__(
-        self, id: int, title: str, author: str, description: str, rating: float
+        self,
+        id: int,
+        title: str,
+        author: str,
+        description: str,
+        rating: float,
+        publish_date: int,
     ):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.publish_date = publish_date
 
 
 class BookRequest(BaseModel):
@@ -23,6 +30,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(max_length=100, min_length=1)
     rating: float = Field(gt=0, lt=6)
+    publish_date: int = Field(gt=1899, lt=3000)
 
     class Config:
         schema_extra = {
@@ -31,14 +39,15 @@ class BookRequest(BaseModel):
                 "author": "person who wrote the book",
                 "description": "one liners about the book",
                 "rating": 4,
+                "publish_date": 2012,
             }
         }
 
 
 BOOKS = [
-    Book(1, "Physic", "Talha", "Applied Physics", 4),
-    Book(2, "Chemistry", "Tahir", "Organic chemistry", 4.5),
-    Book(3, "Computer", "Talha", "Data Structures", 5),
+    Book(1, "Physic", "Talha", "Applied Physics", 4, 2014),
+    Book(2, "Chemistry", "Tahir", "Organic chemistry", 4.5, 2014),
+    Book(3, "Computer", "Talha", "Data Structures", 5, 2014),
 ]
 
 
@@ -60,5 +69,15 @@ def get_books_by_rating(rating: float):
     result_books = list()
     for i in BOOKS:
         if i.rating == rating:
+            result_books.append(i)
+    return result_books
+
+
+# Assigment question to filter books by publish_date
+@app.get("/books/{publish_date}")
+def get_books_by_rating(publish_date: float):
+    result_books = list()
+    for i in BOOKS:
+        if i.publish_date == publish_date:
             result_books.append(i)
     return result_books
