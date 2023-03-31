305e4f0fea9efe608c2823ab5a4ab7b880492c47
Muhammad Talha Tahir <talhatahir@pk.folio3.com>
2023-03-31 19:54:12 +0500

    get_current_user can now be added to any route that requires authoriztaion

--- routers/auth.py
+++ routers/auth.py
@@ -1,14 +1,13 @@
-import sys
-from typing import Any, List
+from datetime import datetime, timedelta
 
 from database_config import local_session
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
 from models import User
 from passlib.context import CryptContext
 from pydantic import BaseModel, Field
-
-sys.path.append("..")
-
+from starlette import status
 
 auth_router = APIRouter(
     prefix="/users", tags=["users"], responses={401: {"user": "Not Authenticated"}}
@@ -24,10 +23,38 @@ def get_db():
 
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth_bearer = OAuth2PasswordBearer(tokenUrl="/login")
+
+SECRET_KEY = "my_super_secret_key"
+ALGO = "HS256"
 
 
 def generate_hash(password: str) -> str:
     return bcrypt_context.hash(password)
+
+
+def verify_pass(password: str, hash_pass: str) -> bool:
+    return bcrypt_context.verify(password, hash_pass)
+
+
+def generate_token(user_id: int, username: str) -> str:
+    expire_time = datetime.now() + timedelta(minutes=30)
+    token = jwt.encode(
+        {"user_id": user_id, "username": username, "exp": expire_time},
+        SECRET_KEY,
+        algorithm=ALGO,
+    )
+    return token
+
+
+async def get_current_user(token: str = Depends(oauth_bearer)) -> dict[str, str]:
+    try:
+        payload = jwt.decode(token=token, key=SECRET_KEY, algorithms=ALGO)
+        user_id = payload.get("user_id")
+        username = payload.get("username")
+        return {"id": user_id, "username": username}
+    except JWTError:
+        raise HTTPException(status_code=401, detail="unauthorized_user")
 
 
 class UserModel(BaseModel):
@@ -47,18 +74,37 @@ class UserModel(BaseModel):
         }
 
 
-@auth_router.get("/", response_model=List[UserModel])
-async def get_all_users(db: local_session = Depends(get_db)) -> List[UserModel]:
+@auth_router.get("/")
+async def get_all_users(db: local_session = Depends(get_db)):
     users = db.query(User).all()
     return users
 
 
-@auth_router.post("/create_new")
+@auth_router.post("/create_new", status_code=status.HTTP_201_CREATED)
 async def create_new_user(
     new_user: UserModel, db: local_session = Depends(get_db)
 ) -> dict:
     user = User(**new_user.dict())
     user.password = generate_hash(new_user.password)
-    db.add(user)
-    db.commit()
-    return {"status": "user added."}
+    try:
+        db.add(user)
+        db.commit()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="user_not_inserted")
+    return {"body": "new_user_created"}
+
+
+@auth_router.post("/login", status_code=status.HTTP_200_OK)
+async def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: local_session = Depends(get_db),
+) -> dict:
+    user = db.query(User).filter(User.username == form_data.username).first()
+    if user == None:
+        raise HTTPException(status_code=404, detail="user_not_found")
+    else:
+        if verify_pass(form_data.password, user.password):
+            token = generate_token(user.id, user.username)
+            return {"access_token": token}
+        else:
+            raise HTTPException(401, detail="invalid_password")
