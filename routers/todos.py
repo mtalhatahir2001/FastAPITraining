017ca6bca4914ba2d907b1dca14c5400294669db
Muhammad Talha Tahir <talhatahir@pk.folio3.com>
2023-04-03 21:34:11 +0500

    added logging to todos.py

--- routers/todos.py
+++ routers/todos.py
@@ -1,3 +1,5 @@
+import logging
+
 from database_config import local_session
 from fastapi import APIRouter, Depends, HTTPException, Path
 from models import Todo
@@ -10,6 +12,7 @@ from .auth import get_current_user, get_db
 todo_router = APIRouter(
     prefix="/todos", tags=["Todos"], responses={401: {"user": "user_not_authenticated"}}
 )
+logging.basicConfig(level=logging.DEBUG, filename="logs.txt")
 
 
 class TodoModel(BaseModel):
@@ -36,6 +39,7 @@ async def get_all_todos(
     Requires the the token to be passed in header.
     """
     if user == None:
+        logging.error(f"invalid_token -- from {__name__}.get_all_todos")
         raise HTTPException(status_code=404, detail="user_not_found")
     else:
         todos = db.query(Todo).filter(Todo.user_id == user.get("id")).all()
@@ -53,6 +57,7 @@ async def get_todo_by_id(
     Requires the the token to be passed in header.
     """
     if user == None:
+        logging.error(f"invalid_token -- from {__name__}.get_todo_by_id")
         raise HTTPException(status_code=404, detail="user_not_found")
     else:
         todo = (
@@ -61,6 +66,7 @@ async def get_todo_by_id(
             .first()
         )
         if todo == None:
+            logging.error(f"invalid_todo_id -- from {__name__}.get_todo_by_id")
             raise HTTPException(status_code=404, detail="todo_not_found")
         result = TodoModel(**todo.__dict__)
         return result
@@ -77,13 +83,18 @@ async def add_todo(
     Requires the the token to be passed in header.
     """
     if user == None:
+        logging.error(f"invalid_token -- from {__name__}.add_todo")
         raise HTTPException(status_code=404, detail="user_not_found")
     new_todo = Todo(**todo.dict())
     new_todo.user_id = user.get("id")
     try:
+        logging.info(
+            f"adding todo{new_todo.title} to db -- from {__name__}.get_todo_by_id"
+        )
         db.add(new_todo)
         db.commit()
     except Exception as e:
+        logging.exception(f"Exception")
         raise HTTPException(status_code=500, detail="todo_not_inserted")
     return {"detail": "new_todo_created"}
 
@@ -100,6 +111,7 @@ async def update_todo(
     Requires the the token to be passed in header.
     """
     if user == None:
+        logging.error(f"invalid_token -- from {__name__}.update_todo")
         raise HTTPException(status_code=404, detail="user_not_found")
     result = (
         db.query(Todo)
@@ -107,15 +119,18 @@ async def update_todo(
         .first()
     )
     if result == None:
+        logging.error(f"invalid_todo -- from {__name__}.update_todo")
         raise HTTPException(status_code=404, detail="todo_not_found")
     else:
         result.title = todo.title
         result.discription = todo.discription
         result.priority = todo.priority
     try:
+        logging.info(f"updating todo{result.id} -- from {__name__}.update_todo")
         db.add(result)
         db.commit()
     except Exception as e:
+        logging.exception(f"Exceptions")
         raise HTTPException(status_code=500, detail="todo_not_inserted")
     return {"detail": "todo_updated"}
 
@@ -131,12 +146,15 @@ async def delete_todo(
     Requires the the token to be passed in header.
     """
     if user == None:
+        logging.error(f"invalid_token -- from {__name__}.delete_todo")
         raise HTTPException(status_code=404, detail="user_not_found")
     db.query(Todo).filter(
         and_(Todo.id == todo_id, Todo.user_id == user.get("id"))
     ).delete()
     try:
+        logging.info(f"deleting todo{todo_id} -- from {__name__}.delete_todo")
         db.commit()
     except Exception as e:
+        logging.exception("Exception")
         raise HTTPException(status_code=500, detail="todo_not_deleted")
     return {"detail": "todo_deleted"}
